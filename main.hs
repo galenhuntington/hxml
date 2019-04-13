1774e16558038c89ede04e765dac81e7a8629add
Galen Huntington <galen@alumni.reed.edu>
2019-04-12 20:28:58 -0700

    Exit status on error.

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import System.IO
+import System.Exit (die)
 import qualified Data.ByteString.Lazy as BL
 import Text.Hxml
 
@@ -6,5 +7,5 @@ main :: IO ()
 main = do
    inp <- BL.getContents
    case parseHxml inp of
-      Left err -> hPutStr stderr $ "error:\n" <> show err <> "\n"
+      Left err -> die $ "error:\n" <> show err <> "\n"
       Right str -> BL.hPut stdout str
